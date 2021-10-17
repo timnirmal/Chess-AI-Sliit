8df57472caed69f567546cebf6ef57aa576381c1
thimira nirmal <timnirmal@gmail.com>
2021-10-17 07:46:15 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var board;
+
+var onDragStart = function (source, piece, position, orientation) {
+
+};
+
+var onDrop = function (source, target) {
+
+};
+
+var onSnapEnd = function () {
+
+};
+
+var onMouseoverSquare = function(square, piece) {
+
+};
+
+var onMouseoutSquare = function(square, piece) {
+
+};
+
+var config = {
+
+};
+
+board = ChessBoard('board', config);
