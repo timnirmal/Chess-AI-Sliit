9f1ee6989f1e1a81decef53e140ad2d7e9853354
thimira nirmal <timnirmal@gmail.com>
2021-10-30 17:45:41 +0530

    Play with Other Configurations before moving to game

--- script.js
+++ script.js
@@ -23,6 +23,17 @@ var onMouseoutSquare = function(square, piece) {
 var config = {
     position: 'start',
     draggable: true,
+    orientation: 'white', //Change Orientation (Player Plays Black/White)
+    showNotation: false, //Show Notation true/false
+    dropOffBoard: 'trash', //Drop off board 'trash'/'snapback'
+
+    //Change move speed
+    moveSpeed: 'slow',
+    snapbackSpeed: 500,
+    snapSpeed: 100,
+
+    // You can check other configuration on
+    https://chessboardjs.com/examples#1000
 };
 
 board = ChessBoard('board', config);
